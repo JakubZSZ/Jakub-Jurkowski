fe901b6e4b5b5cc141189edfc08a15a976f5d8c4
JakubZSZ <jakub.jurkowski@zsz.bobowa.pl>
2022-09-30 14:23:26 +0200

    v4

---
+++ Podzespoły komputerowe/index.php
@@ -0,0 +1,48 @@
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl8.css" type="text/css">
+    <title>Nasz sklep komputerowy</title>
+</head>
+<body>
+    <div class="blok.menu">
+<a href="index.php">Główna</a>
+<a href="procesory.html">Procesory</a>
+<a href="ram.html">RAM</a>
+<a href="grafika.html">Grafika</a>
+</div>
+<div class="blok.logo">
+<h2>Podzespoły komputerowe</h2>
+</div>
+<div class="blok.menu">
+<h1>Dzisiejsze promocje</h1>
+<table>
+    <th>NUMER</th><th>NAZWA PODZESPOŁU</th><th>OPIS</th><th>CENA</th>
+    <td> </td><td> </td><td> </td><td> </td>
+    <td> </td><td> </td><td> </td><td> </td>
+    <td> </td><td> </td><td> </td><td> </td>
+    <td> </td><td> </td><td> </td><td> </td>
+    <td> </td><td> </td><td> </td><td> </td>
+    <td> </td><td> </td><td> </td><td> </td>
+    <td> </td><td> </td><td> </td><td> </td>
+    <td> </td><td> </td><td> </td><td> </td>
+    <td> </td><td> </td><td> </td><td> </td>
+</table>
+</div>
+<footer>
+<img src="scalak.jpg" alt="Nasz Sklep Komputerowy">
+</footer>
+    <footer>
+<h4>Nasz Sklep Komputerowy</h4>
+<p>Współpracujemy z hurtownią <a href="http://www.edata.pl/">edata</a></p>
+</footer>
+    <footer>
+<h3>zadzwoń: 000 000 000
+</footer>
+    <footer>
+<h4>Stronę wykonał: 00000000000</h4>
+</footer>
+</body>
+</html>
