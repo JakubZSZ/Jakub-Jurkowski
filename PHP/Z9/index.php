f7d90b96d32b21b560670a7f46619a2878a5cd21
JakubZSZ <jakub.jurkowski@zsz.bobowa.pl>
2022-10-24 13:49:10 +0200

    v5

---
+++ PHP/Z9/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprawdź</title>
+</head>
+<body>
+    <form acrion="" method="post">
+    a: <input type="number" name="a">
+    <input type="submit" value="Sprawdź">
+</form>
+<?php
+if (isset($_POST['a'])){
+    $a = $_POST['a'];
+
+    if ($a>0){
+        echo "$a > 0";
+        }
+    if ($a<0){
+        echo "$a < 0";
+    }
+    if ($a==0){
+        echo "$a = 0";
+    }
+}
+?>
+</body>
+</html>
