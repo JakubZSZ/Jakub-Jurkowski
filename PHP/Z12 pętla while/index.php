439a3a186347d874968715e5195234cd90c920c2
JakubZSZ <jakub.jurkowski@zsz.bobowa.pl>
2022-10-28 13:38:16 +0200

    v1

---
+++ PHP/Z12 pętla while/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+    echo "I pętla: <br>";
+    $x = 1;
+    while ($x <= 5) {
+        echo "$x, ";
+        $x++;
+    }
+    echo "<br><br>";
+    echo "II pętla: <br>";
+    $x = 10;
+    while ($x >= 0) {
+        echo "$x, ";
+        $x-=2;
+    }
+
+?>
+
+</body>
+</html>
