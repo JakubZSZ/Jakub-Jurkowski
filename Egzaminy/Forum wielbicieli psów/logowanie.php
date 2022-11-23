ff5274f98e6acb16b48567d47b3b739bbd3ff828
JakubZSZ <jakub.jurkowski@zsz.bobowa.pl>
2022-11-23 12:22:12 +0100

    v4

--- Egzaminy/Forum wielbicieli psów/logowanie.php
+++ Egzaminy/Forum wielbicieli psów/logowanie.php
@@ -17,12 +17,43 @@
     <div class="bprawy">
         <h2>Zapisz się</h2>
         <form action="" method="post">
-            login: <input type="text" class="login">
-            hasło: <input type="password" class="haslo">
-            powtórz hasło: <input type="password" class="phaslo">
+            login: <input type="text" name="login"><br>
+            hasło: <input type="password" name="haslo"><br>
+            powtórz hasło: <input type="password" name="phaslo"><br>
             <input type="sumbit" value="Zapisz">
             <?php 
+                if (isset($_POST['login']) && 
+                    isset($_POST['haslo']) && 
+                    isset($_POST['phaslo'])) {
+                    
+                    $log = $_POST['login'];
+                    $hasl = $_POST['haslo'];
+                    $phasl = $_POST['phaslo'];
+                    
+                    $con = mysqli_connect('localhost','root','','psy');
 
+                    if ($log ==""  || $hasl =="" || $phasl =="") {
+                        echo "<p>wypełnij wszystkie pola</p>";
+                    }
+
+                    $sql = "SELECT login FROM `uzytkownicy` WHERE $log;";
+                    $result = mysqli_query($con,$sql);
+                    while ($value = mysqli_num_row($result)) {
+                        if ($value > 0) {
+                            echo "<p>login występuje w bazie danych</p>"; 
+                        }                   
+                    }
+                    
+                    if ($hasl != $phasl) {
+                        echo "<p>hasła nie są takie same, konto nie zostało dodane</p>";
+                    }
+
+                    
+
+                    
+
+                    mysqli_close($con);
+                }
             ?>
         </form>
     </div>
@@ -37,7 +68,7 @@
         <a href="regulamin.html">Przeczytaj regulamin forum</a>
     </div>
     <div class="stopka">
-        <p>Stronę wykonał: 00000000000</p>
+        Stronę wykonał: 00000000000
     </div>
 </body>
 </html>
