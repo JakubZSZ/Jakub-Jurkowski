ad0ed1e5fb5906f3189ea456e6c3a9a980bc311d
JakubZSZ <jakub.jurkowski@zsz.bobowa.pl>
2022-11-23 11:32:53 +0100

    v3

---
+++ Egzaminy/Forum wielbicieli psów/logowanie.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl4.css">
+    <title>Forum o psach</title>
+</head>
+<body>
+    <div class="baner">
+        <h1>Forum wielbicieli psów</h1>
+    </div>
+    <div class="blewy">
+        <img src="obraz.jpg" alt="foksterier">
+    </div>
+    <div class="bprawy">
+        <h2>Zapisz się</h2>
+        <form action="" method="post">
+            login: <input type="text" class="login">
+            hasło: <input type="password" class="haslo">
+            powtórz hasło: <input type="password" class="phaslo">
+            <input type="sumbit" value="Zapisz">
+            <?php 
+
+            ?>
+        </form>
+    </div>
+    <div class="prawy">
+        <h2>Zapraszamy wszystkich</h2>
+    </div>
+    <div class="stopka">
+
+    </div>
+</body>
+</html>
